40a987a3438456fdc3bee45cb2ac057e06e0c758
Marco de Boer <128293216+GitMarcoo@users.noreply.github.com>
2023-12-18 21:16:08 +0100

    init

---
+++ green_1backend/src/main/java/app/Green1backendApplication.java
@@ -0,0 +1,27 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Green1backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Green1backendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String allowedOrigin = System.getenv("ALLOWED_ORIGIN");
+                registry.addMapping("/**").allowedOrigins(allowedOrigin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
---
+++ src/CONFIG.js
@@ -0,0 +1,3 @@
+export const CONFIG = {
+  BACKEND_URL: process.env.VUE_APP_API_URL
+}
